56413d00d3ecef7e3daadd8df8b2d973c0b9a5ef
Jeffrey <Jefferymsd101@gmail.com>
2026-07-10 11:11:18 +0530

    Add User model with shopping cart

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    required: true,
+    unique: true,
+    type: String
+  },
+  email: {
+    required: true,
+    unique: true,
+    type: String
+  },
+  password: {
+    required: true,
+    type: String
+  },
+  cart: [{
+    productId:{
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product"
+    },
+    quantity: {
+      required: true,
+      type: Number
+    }
+  }]
+})
+const User = mongoose.model("User", userSchema);
+module.exports = User
