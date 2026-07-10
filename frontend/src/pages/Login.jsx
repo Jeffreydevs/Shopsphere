892c44639dbebdf9eae1941ea6f9b4a4daa489cb
Jeffrey <Jefferymsd101@gmail.com>
2026-07-11 00:50:46 +0530

    Implement user login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,35 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
-  return(
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
 
-  )     
+  const API_URL = "http://localhost:3000";
+    
+  async function handleLogin(){
+      if(!email || !password){
+        alert("Please fill all fields")
+        return
+      }
+      try{
+        const response = await axios.post(`${API_URL}/login`,{email,password});
+        localStorage.setItem("token", response.data.token);
+        alert("Login successful!");
+        navigate("/");
+      }
+      catch(error){
+        alert(error.response.data.message);
+      }
+   }
+
+  return(
+      <div>
+        <input type="email" placeholder="Enter email" value={email} onChange={(event)=> setEmail(event.target.value)}/>
+        <input type="password" placeholder="Enter password" value={password} onChange={(event)=> setPassword(event.target.value)}/>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+   )     
 }
 export default Login
