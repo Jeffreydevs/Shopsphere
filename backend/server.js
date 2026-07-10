9bcc6e9518425fb9a89c5c4c0dbff66b1b43dbb3
Jeffrey <Jefferymsd101@gmail.com>
2026-07-10 11:41:21 +0530

    Implement user registration with password hashing

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Product = require("./models/Product");
+const User = require("./models/User")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -40,13 +41,40 @@ app.post("/products", async(req,res) => {
        description,
        stock
       });
-      console.log(product)
       return res.status(201).json({ message: "Product created successfully" });
     }
   catch(error){
       console.log(error)
       return res.status(500).json({ message: "Something went wrong" });
    }
+});
+
+app.post("/register", async(req,res) => {
+  try{
+      const{username, email, password} = req.body
+      if(!username || !email || !password){
+       return res.status(400).json({ message: "Please fill all fields" });
+      }
+      const existingUsername = await User.findOne({username})
+      if(existingUsername){
+        return res.status(409).json({ message: "Username already exists"});
+      }
+      const existingEmail = await User.findOne({email})
+      if(existingEmail){
+        return res.status(409).json({ message: "Email already exists"});
+      }
+      const hashedPassword = await bcrypt.hash(password,10)
+      await User.create({
+        username,
+        email,
+        password: hashedPassword
+      })
+      return res.status(201).json({ message: "User created successfully" });
+    }    
+    catch(error){
+      console.log(error)
+      return res.status(500).json({ message: "Something went wrong" });
+    }
 });
 
 mongoose.connect(process.env.MONGO_URI) 
